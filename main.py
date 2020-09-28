53b863a5381af71d67a42ddaca554fe7501cee61
unknown <18it091@charusat.edu.in>
2020-09-28 14:11:58 +0530

    Created:main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 
 from PyQt5.QtWidgets import QApplication
 from view import GUI
+from controller import Controller
 from model import evaluateExpression
 
 # Client code
@@ -13,6 +14,8 @@ def main():
     # Show the calculator's GUI
     view = GUI()
     view.show()
+    model = evaluateExpression
+    Controller(model=model,view=view)
 
     sys.exit(pycalc.exec_())
 
